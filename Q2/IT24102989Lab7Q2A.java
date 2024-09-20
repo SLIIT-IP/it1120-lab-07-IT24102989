6d9767961751986d1153585ba4195a4c20e4fa69
IT24102989 <it24102989@my.sliit.lk>
2024-09-20 12:06:26 +0530

    Add IT24102989Lab7Q2A to Q2A

---
+++ Q2/IT24102989Lab7Q2A.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+ public class IT24102989Lab7Q2A {
+   public static void main(String[] args){
+ 
+     int rows = 4;
+     int colomns = 5;
+ 
+     for (int i = 0 ; i < rows; i++) {  
+
+     for (int j = 0 ; j < colomns; j++) {
+      System.out.print("$ ");
+
+     }
+    
+     System.out.println();
+    }
+  }
+}
